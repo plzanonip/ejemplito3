44ecd0cd3bfeddf32660b0410a63ecceb02bd9b5
plzanonip <plzanonip@gmail.com>
2025-04-15 08:34:25 -0500

    Commit1

---
+++ src/test/java/pe/edu/upc/demoejemplito3/DemoEjemplito3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.demoejemplito3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEjemplito3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
